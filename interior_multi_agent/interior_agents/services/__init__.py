dc65b0184af0bf67dc828f6194eb768f6b6dac79
kadykwon <gncloud86@naver.com>
2025-06-14 00:03:09 +0900

    최신

--- interior_multi_agent/interior_agents/services/__init__.py
+++ interior_multi_agent/interior_agents/services/__init__.py
@@ -4,9 +4,6 @@ Business logic services for interior design management system.
 This package contains services that handle complex business operations.
 """
 
-# 현장 관리 서비스
-from .site_manager import register_site, get_site_info, list_all_sites
-
 # 공사 분할 지급 계획 서비스
 from .construction_payment_planner import (
     request_site_address,
@@ -19,11 +16,6 @@ from .construction_payment_planner import (
 )
 
 __all__ = [
-    # 현장 관리
-    'register_site',
-    'get_site_info', 
-    'list_all_sites',
-    
     # 공사 분할 지급 계획
     'request_site_address',
     'make_payment_plan',
