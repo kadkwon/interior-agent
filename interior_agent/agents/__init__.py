86a2cdf8943d574b593202a05310d19846b1da94
kadykwon <gncloud86@naver.com>
2025-07-06 12:12:14 +0900

    최신

--- interior_agent/agents/__init__.py
+++ interior_agent/agents/__init__.py
@@ -4,15 +4,18 @@
 ADK 표준 구조에 따른 하위 에이전트들:
 - firebase_agent: Firebase Firestore 전문 처리
 - email_agent: 이메일 전송 및 서버 관리
+- as_agent: 친절한 고객 AS 응대
 - formatter_agent: 응답 포맷팅 도구
 """
 
 from .firebase_agent import firebase_agent
 from .email_agent import email_agent
+from .as_agent import as_agent
 from .formatter_agent import format_korean_response
 
 __all__ = [
     'firebase_agent',
-    'email_agent', 
+    'email_agent',
+    'as_agent',
     'format_korean_response'
 ] 
